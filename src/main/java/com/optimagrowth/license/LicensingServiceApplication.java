28d9d3242f73c44b7af100cae70fc297349daea4
Andrey <and64region2015@gmail.com>
2022-09-19 21:21:14 +0300

    init, actuator, hateoas, plugin docker and dockerfile

---
+++ src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -0,0 +1,35 @@
+package com.optimagrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		Locale defaultLocale = new Locale("ru");
+		slr.setDefaultLocale(defaultLocale);
+		return slr;
+	}
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setCacheSeconds(3600);
+		messageSource.setDefaultEncoding("UTF-8"); // Add this
+		return messageSource;
+	}
+}
